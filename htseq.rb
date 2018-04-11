51789a81e6d85ca336424705d4a87fce084ee383
kojimaryuta <axha0838@chiba-u.jp>
2018-04-11 23:35:14 +0900

    Revert "first commit"
    This reverts commit 1ad945319e3d72317418d096cad59fc4e81cfe15.

--- htseq.rb
+++
@@ -1,9 +0,0 @@
-require "parallel"
-files = Dir.glob("bam/*_sorted.bam").sort
-
-Parallel.each(files, in_processes:12) do |f|
-  base = File.basename(f, "_sorted.bam")
-  s = "htseq-count -f bam #{f} mm10_gtf/Annotation/Genes/genes.gtf > rep2/#{base}.txt"
-  puts s
-  system s
-end
--- index.rb
+++
@@ -1,9 +0,0 @@
-require 'parallel'
-
-files = Dir.glob("bam/*_sorted.bam").sort
-
-Parallel.each(files, in_processes:16) do |f|
-  s = "samtools index #{f}"
-  puts s
-  system s
-end
