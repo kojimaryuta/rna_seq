976f8362753da66ad0c44c235f335eb3987b8118
kojimaryuta <axha0838@chiba-u.jp>
2018-05-05 15:42:18 +0900

    add comments to index.rb

--- index.rb
+++ index.rb
@@ -1,9 +1,16 @@
+# IGV(ゲノムブラウザ)で閲覧するためのindexを作成する
+
+# Ruby でマルチプロセスするためのライブラリ
 require 'parallel'
 
+# bam ディレクトリ以下の _sorted.bam ファイルを処理
 files = Dir.glob('bam/*_sorted.bam').sort
 
+# CPU 16 コア
 Parallel.each(files, in_processes: 16) do |f|
+  # コマンド
   s = "samtools index #{f}"
-  puts s
-  system s
+
+  puts s   # コマンド出力
+  system s # 実行
 end
