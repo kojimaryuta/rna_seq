ce0b10400f65d7fac858a133f90fdbcdba4ab912
kojimaryuta <axha0838@chiba-u.jp>
2018-05-05 15:11:56 +0900

    add comments to qc2.rb

--- qc2.rb
+++ qc2.rb
@@ -1,8 +1,15 @@
+# prinseq-lite でクオリティコントロール
+
 require 'parallel'
+
+# ディレクトリ内のfastqファイルについて操作
 files = Dir.glob('*.fastq').sort
 
 Parallel.each(files, in_proesses: 8) do |f|
   base = File.basename(f, '.fastq')
+
+  # prinseq-lite コマンド
+  # オプションはアメリエフ資料と同じにしてるだけ
   s = "prinseq-lite.pl -fastq #{f} " \
       "-out_good qc2/#{base}.notail " \
       "-out_bad qc2/#{base}.bad " \
@@ -12,7 +19,11 @@ Parallel.each(files, in_proesses: 8) do |f|
       '-trim_qual_right 30 ' \
       '-ns_max_p 20 ' \
       '-min_len 30 ' \
-      "2> qc2/#{base}.txt"
+      "2> qc2/#{base}.txt" # 標準エラー出力をファイルに保存
+
+  # コンソールへコマンド出力
   puts s
+
+  # 実行
   system s
 end
